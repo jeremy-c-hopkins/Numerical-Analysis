c087a09304a9bd461bd37cdfd1a64a5a1e18d727
iris <0cokasprite0@gmail.com>
2024-02-22 19:41:27 -0700

    fix the minor error

--- m5600/jacobi_gauss-seidel.cpp
+++ m5600/jacobi_gauss-seidel.cpp
@@ -12,7 +12,7 @@ int main(){
     int yEigen;
     int iter;
 
-    cout << "Enter the node count (n^2): " << endl;
+    cout << "Enter the node count (which equals to n^2): " << endl;
     cin >> n;
     cout << "Enter x eigen value: " << endl;
     cin >> xEigen;
@@ -31,29 +31,28 @@ int main(){
         for (int l=0;l<n;l++) {
  		    arr[i][l]=0.0;
  		} 
-       // arr[i][i] = -4.0;
+        arr[i][i] = -4.0;
 
         
+        if(i-1>-1 && i%sqrtN!=0){
+            arr[i][i-1]=1.0;
+        }
+        if(i+1<n && ((i+1)%sqrtN)!=0){
+            arr[i][i+1]=1.0;
+        }
 
-        // if(i-1>-1 && i%sqrtN!=0){
-        //     arr[i][i-1]=1.0;
-        // }
-        // if(i+1<n && ((i+1)%sqrtN)!=0){
-        //     arr[i][i+1]=1.0;
-        // }
-
-        // for(int j=0; j<n; j++){
-        //     if(j-sqrt(n)==i){
-        //         arr[i][j]=1.0;
-        //     }
-        //     if(j+sqrt(n)==i){
-        //         arr[i][j]=1.0;
-        //     }
-        //     if(abs(arr[i][j])<0.1){
-        //         arr[i][j]=0;
-        //     }
-        // }
-        // f[i]=sin(xEigen*(i+1)*h)*sin(yEigen*(i+1)*h);
+        for(int j=0; j<n; j++){
+            if(j-sqrt(n)==i){
+                arr[i][j]=1.0;
+            }
+            if(j+sqrt(n)==i){
+                arr[i][j]=1.0;
+            }
+            if(abs(arr[i][j])<0.1){
+                arr[i][j]=0;
+            }
+        }
+         f[i]=sin(xEigen*(i+1)*h)*sin(yEigen*(i+1)*h);
     }
 
     for(int i=0; i<n; i++){
